713385fe410fd0ba9c816feb5944cc1043e1f06d
Anissa <anissa.evrd@gmail.com>
2025-09-24 18:59:13 +0200

    test

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,24 +1,24 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 // Créer une connexion à la base de données
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.MYSQLHOST, // Utilise le host de Railway
   user: process.env.MYSQLUSER, // Utilise l'utilisateur MySQL (root dans ton cas)
   password: process.env.MYSQL_ROOT_PASSWORD, // Mot de passe de la base de données
   database: process.env.MYSQL_DATABASE, // Le nom de ta base de données
-  port: 28949, // Port MySQL
+  port: 3306, // Port MySQL
   connectTimeout: 20000, // Timeout de 20 secondes
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.error("Erreur de connexion à la base de données:", err);
-    return;
-  }
-  console.log("Connecté à la base de données MySQL");
-});
+// connection.connect((err) => {
+//   if (err) {
+//     console.error("Erreur de connexion à la base de données:", err);
+//     return;
+//   }
+//   console.log("Connecté à la base de données MySQL");
+// });
 
 export default connection;

1373768589652ea4c5d624a402a196cac4f38fd2
Anissa <anissa.evrd@gmail.com>
2025-09-24 20:43:44 +0200

    Revert "test"
    This reverts commit 713385fe410fd0ba9c816feb5944cc1043e1f06d.

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,27 +1,24 @@
-import mysql from "mysql2/promise";
+import mysql from "mysql2";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 // Créer une connexion à la base de données
-const connection = mysql.createPool({
+const connection = mysql.createConnection({
   host: process.env.MYSQLHOST, // Utilise le host de Railway
   user: process.env.MYSQLUSER, // Utilise l'utilisateur MySQL (root dans ton cas)
   password: process.env.MYSQL_ROOT_PASSWORD, // Mot de passe de la base de données
   database: process.env.MYSQL_DATABASE, // Le nom de ta base de données
-  port: 3306, // Port MySQL
+  port: 28949, // Port MySQL
   connectTimeout: 20000, // Timeout de 20 secondes
 });
-console.log("MYSQLHOST:", process.env.MYSQLHOST);
-console.log("MYSQLPORT:", process.env.MYSQLPORT);
-console.log("MYSQLUSER:", process.env.MYSQLUSER);
-console.log("MYSQLDATABASE:", process.env.MYSQLDATABASE, process.env.MYSQL_DATABASE);
-// connection.connect((err) => {
-//   if (err) {
-//     console.error("Erreur de connexion à la base de données:", err);
-//     return;
-//   }
-//   console.log("Connecté à la base de données MySQL");
-// });
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Erreur de connexion à la base de données:", err);
+    return;
+  }
+  console.log("Connecté à la base de données MySQL");
+});
 
 export default connection;

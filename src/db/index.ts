d356fe87ee7f819a79d2267fd13554dd73d1b3ac
Anissa <anissa.evrd@gmail.com>
2026-02-05 22:21:18 +0100

    feat(db): Migrate to MySQL connection pool

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,22 +3,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// Créer une connexion à la base de données
-const connection = mysql.createConnection({
+// Créer un pool de connexions à la base de données
+const connection = mysql.createPool({
   host: process.env.MYSQLHOST, // Utilise le host de Railway
   user: process.env.MYSQLUSER, // Utilise l'utilisateur MySQL (root dans ton cas)
   password: process.env.MYSQL_ROOT_PASSWORD, // Mot de passe de la base de données
   database: process.env.MYSQL_DATABASE, // Le nom de ta base de données
   port: Number(process.env.MYSQLPORT ?? process.env.MYSQL_PORT ?? 28949), // Port MySQL
   connectTimeout: 20000, // Timeout de 20 secondes
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error("Erreur de connexion à la base de données:", err);
-    return;
-  }
-  console.log("Connecté à la base de données MySQL");
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 export default connection;
